f56bdded0e58f59ce3157198dc08f1f48b088522
rodguzleonardo <rodguz.leonardo@gmail.com>
2026-05-18 17:40:09 -0600

    Primer Commit

---
+++ target/debug/deps/ActixWebTaskService-1a910072a96daeb1.d
@@ -0,0 +1,5 @@
+/home/leonardorg/portafolio/rust/ActixWebBackend/ActixWebTaskService/target/debug/deps/ActixWebTaskService-1a910072a96daeb1.d: src/main.rs
+
+/home/leonardorg/portafolio/rust/ActixWebBackend/ActixWebTaskService/target/debug/deps/libActixWebTaskService-1a910072a96daeb1.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/ActixWebTaskService-c598352c6fa3e631.d
@@ -0,0 +1,5 @@
+/home/leonardorg/portafolio/rust/ActixWebBackend/ActixWebTaskService/target/debug/deps/ActixWebTaskService-c598352c6fa3e631.d: src/main.rs
+
+/home/leonardorg/portafolio/rust/ActixWebBackend/ActixWebTaskService/target/debug/deps/libActixWebTaskService-c598352c6fa3e631.rmeta: src/main.rs
+
+src/main.rs:
